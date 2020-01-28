690eb80e34930b0b3d8d55bb99807776c328e86f
Ec_@PC <dmatis01@gmail.com>
2020-01-28 22:55:13 +0100

    Added loaders to page and modals

---
+++ frontend/src/components/LoadingModal/LoadingModal.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Modal, Spinner } from "react-bootstrap";
+import "./LoadingModal.scss";
+import { GrowLoader } from "../GrowLoader";
+
+export class LoadingModal extends React.Component {
+    render() {
+        return <Modal
+            show={true}
+            size="lg"
+            centered
+            onHide={() => { }}
+            className="loading-modal"
+        >
+            <GrowLoader size={4} />
+        </Modal>
+    }
+}
