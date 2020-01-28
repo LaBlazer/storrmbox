e748e08256e371395254333706126028183a5ba1
Ec_@PC <dmatis01@gmail.com>
2020-01-28 23:02:42 +0100

    Added version to footer

--- frontend/src/layout/Footer/Footer.tsx
+++ frontend/src/layout/Footer/Footer.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import "./Footer.scss";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { VERSION, GIT_COMMIT_HASH } from '../../configs/constants';
 
 export class Footer extends React.Component {
     render() {
@@ -9,6 +10,8 @@ export class Footer extends React.Component {
             <a href="https://github.com/LaBlazer/storrmbox" target="__blank" className="name">
                 <FontAwesomeIcon icon={faGithub} /> Storrmbox
             </a>
+            &nbsp;&nbsp;|&nbsp;&nbsp;
+            <span className="version">{`${VERSION}.${GIT_COMMIT_HASH}`}</span>
         </div>
     }
 }
