75204c183fefc9f9938e6ed95adda65559963a82
Ec_@PC <dmatis01@gmail.com>
2020-01-30 14:22:14 +0100

    Created Modal link component

---
+++ frontend/src/components/ModalLink.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link, withRouter, RouteComponentProps } from 'react-router-dom';
+
+type MLProps = RouteComponentProps<any, any, { background: string }> & {
+    to: string,
+    [index: string]: any
+};
+
+/**
+ * Used for creating links to modals. Useful for saving background location
+ *
+ * @class ModalLink
+ * @extends {React.Component<MLProps>}
+ */
+class ModalLink extends React.Component<MLProps> {
+
+    render() {
+        let { to, location, history, match, staticContext, ...props } = this.props;
+        let background = location.state?.background ?? location;
+
+        return <Link to={{ pathname: to, state: { background } }} {...props}>
+            {this.props.children}
+        </Link>
+    }
+
+}
+
+export default withRouter(ModalLink);
