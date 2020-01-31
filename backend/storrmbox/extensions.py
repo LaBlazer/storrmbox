e1cf58ea3844dfb6ac5c3ad8004320e8d6b78ae7
Andrej Vojtus <avojtus@gmail.com>
2020-01-31 23:59:31 +0100

    Changed reverted jwt auth type change

--- backend/storrmbox/extensions.py
+++ backend/storrmbox/extensions.py
@@ -5,13 +5,13 @@ from flask_sqlalchemy.model import BindMetaMixin, Model
 from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
 
 
+# Disable Table Name Generation
 class NoNameMeta(BindMetaMixin, DeclarativeMeta):
     pass
 
 
-# Disable Table Name Generation
 db = SQLAlchemy(model_class=declarative_base(cls=Model, metaclass=NoNameMeta, name='Model'))
-basic_auth = HTTPBasicAuth(scheme='Token')
-auth = HTTPTokenAuth(scheme='Token')
+basic_auth = HTTPBasicAuth(scheme='Auth')
+auth = HTTPTokenAuth()
 multi_auth = MultiAuth(basic_auth, auth)
 migrate = Migrate()
