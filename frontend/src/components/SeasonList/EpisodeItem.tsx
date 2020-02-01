5d05eb2f6b4d5bdb9a374642f0b95343b1070358
Ec_@PC <dmatis01@gmail.com>
2020-02-01 18:51:38 +0100

    Added custom modals to every content type

--- frontend/src/components/SeasonList/EpisodeItem.tsx
+++ frontend/src/components/SeasonList/EpisodeItem.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { ListGroup } from 'react-bootstrap';
 import "./SeasonList.scss"
 import { getEpisodeStringRepresentation } from '../../utils/string-formater';
+import ModalLink from '../ModalLink';
 
 type EIProps = {
     season: number,
@@ -14,14 +15,16 @@ type EIProps = {
 
 export class EpisodeItem extends React.Component<EIProps> {
     render() {
-        let { season, episode, title, active } = this.props;
+        let { season, episode, title, active, uid } = this.props;
 
-        return <ListGroup.Item className="item" action active={active}>
-            <div className="mr-auto">
-                <small className="episode-number">{getEpisodeStringRepresentation(season, episode)}</small>
-                <div>{title}</div>
-            </div>
-            <span className="episode-number">#{episode}</span>
-        </ListGroup.Item>
+        return <ModalLink to={`/m/${uid}`} style={{ color: "inherit", textDecoration: "none" }}>
+            <ListGroup.Item className="item" action active={active}>
+                <div className="mr-auto">
+                    <small className="episode-number">{getEpisodeStringRepresentation(season, episode)}</small>
+                    <div>{title}</div>
+                </div>
+                <span className="episode-number">#{episode}</span>
+            </ListGroup.Item>
+        </ModalLink>
     }
 }
