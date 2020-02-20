08b93a94dc0a971b04d9f764deb5f1884bcffb67
Ec_@PC <dmatis01@gmail.com>
2020-02-20 13:47:29 +0100

    Fixed slider's strange behaviour on it's end

--- frontend/src/components/MediaCarousel/AutoSizingAnimatedList.jsx
+++ frontend/src/components/MediaCarousel/AutoSizingAnimatedList.jsx
@@ -85,11 +85,9 @@ export class AutoSizingAnimatedList extends Component {
             const itemsPerScreen = Math.round(this.state.listWidth / this.state.itemWidth);
 
             let scrollTo = this.state.scrollToItem + itemsPerScreen * this.props.scrollDirection;
-            if (scrollTo >= 0 && scrollTo <= this.props.itemCount - itemsPerScreen) {
-                this.moveTo(scrollTo);
-            } else {
-                if (this.props.onAnimationComplete) this.props.onAnimationComplete();
-            }
+            scrollTo = Math.min(this.props.itemCount - itemsPerScreen, Math.max(0, scrollTo));
+
+            this.moveTo(scrollTo);
         }
     }
 
