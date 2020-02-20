58823e3ed79ea07fdd5a0cfcdb5c2a0991ec3371
Ec_@PC <dmatis01@gmail.com>
2020-02-20 18:19:26 +0100

    Added new xxl breakpoint

--- frontend/src/components/MediaCarousel/MediaListSlider.jsx
+++ frontend/src/components/MediaCarousel/MediaListSlider.jsx
@@ -65,7 +65,7 @@ class MediaListSlider extends React.Component {
     }
 
     render() {
-        let listSize = 3, empty = true;
+        let listSize = 4, empty = true;
         if (this.props.uidList && this.props.uidList.length !== 0) {
             listSize = this.props.uidList.length;
             empty = false;
