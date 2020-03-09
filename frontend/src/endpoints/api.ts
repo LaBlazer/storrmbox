5b88efcb9812c9d03f73b88f170c3fbe9ce28d47
Ec_@PC <dmatis01@gmail.com>
2020-03-09 23:04:57 +0100

    Added custom error handling option instead of global  notification

--- frontend/src/endpoints/api.ts
+++ frontend/src/endpoints/api.ts
@@ -1,8 +1,9 @@
 import Axios from "axios";
 import { API_URL } from "../configs/constants";
+import { MyAxiosInstance } from "MyAxios";
 
 const AxiosI = Axios.create({
     baseURL: API_URL
-});
+}) as MyAxiosInstance;
 
 export default AxiosI;
