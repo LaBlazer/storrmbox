a784c71795f1952f21581a0d39786dfe37469b0c
Ec_@PC <dmatis01@gmail.com>
2020-03-11 21:05:02 +0100

    Fixed content reloading not working in admin panel

--- frontend/src/layout/pages/AccountPage/pages/AdminPanel.tsx
+++ frontend/src/layout/pages/AccountPage/pages/AdminPanel.tsx
@@ -1,20 +1,34 @@
+import { observer } from "mobx-react";
 import React, { Component } from "react";
 import { Button } from "react-bootstrap";
-import ContentReloader from "components/ContentReloader";
-import UserStore from "stores/UserStore";
 import { Redirect } from "react-router-dom";
-import { observer } from "mobx-react";
+import UserStore from "stores/UserStore";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRadiation } from "@fortawesome/free-solid-svg-icons";
+import { ContentService } from "endpoints/content";
 
 @observer
 export class AdminPanel extends Component {
-    render() {
-        if(!UserStore.user?.permission) {
-            return <Redirect to='/account'/>
+
+    // @TODO delete this in next versions
+    reloadContent__UNSAFE = () => {
+        var r = window.confirm("Are you sure you want to realod content ? This will delete all cached data from the database!");
+        if (r === true) {
+            console.warn("Sending Reload request for content reloading!!!");
+            ContentService.__reloadContent_UNSAFE();
         }
-        
+    }
+
+    render() {
+        if (!UserStore.user?.permission) {
+            return <Redirect to='/account' />
+        }
+
         return <div>
             <h2>Admin Panel</h2>
-            <Button variant="danger" onClick={() => <ContentReloader />}>Reload content</Button>
+            <Button variant="danger" onClick={this.reloadContent__UNSAFE}>
+                <FontAwesomeIcon icon={faRadiation} />&nbsp;&nbsp;Reload content
+            </Button>
         </div>;
     }
 }
