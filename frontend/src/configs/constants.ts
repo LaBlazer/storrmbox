4df4fc418ed5a39d037749e1bd25ef19d00a01a9
Andrej Vojtuš <avojtus@gmail.com>
2020-03-14 19:51:32 +0100

    Improved mobile UI & UX

--- frontend/src/configs/constants.ts
+++ frontend/src/configs/constants.ts
@@ -1,6 +1,6 @@
-export const VERSION = '0.1';
+export const VERSION = '0.2';
 export const GIT_COMMIT_HASH = process.env.REACT_APP_GIT_COMMIT_HASH || 'dev';
-export const API_URL = process.env.REACT_APP_API_URL;
+export const API_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1/api/v1';
 
 export const TOKEN_COOKIE_NAME = 'atkn';
 export const REMEMBER_ME_COOKIE_NAME = 'rm';
